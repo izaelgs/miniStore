e53ec7ffc775f3606d48a52a48b610dcf6937f6d
izaelgs <izael-123@outlook.com>
2022-12-09 10:55:55 -0300

    adiciona rotas de usuario

---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Api\ApiMessages;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\UserRequest;
+use App\Models\User;
+
+class UserController extends Controller
+{
+
+    private $user;
+
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+    }
+
+    public function index() {
+
+        try {
+            $user = $this->user->paginate('10');
+
+            return response()->json($user, 200);
+        } catch (\Exception $e) {
+            return response()->json($e->getMessage(), 401);
+            // $message = new ApiMessages($e->getMessage());
+            // return response()->json($message->getMessage(), 401);
+        }
+    }
+
+    public function show($id) {
+
+        try {
+            $user = $this->user->findOrFail($id);
+
+            return response()->json($user, 200);
+
+        } catch (\Exception $e) {
+            return response()->json($e->getMessage(), 401);
+            // $message = new ApiMessages($e->getMessage());
+            // return response()->json($message->getMessage(), 401);
+        }
+
+    }
+
+    public function store(UserRequest $request) {
+
+        $data = $request->all();
+
+        try {
+
+            $data['password'] = bcrypt($data['password']);
+            $user = $this->user->create($data);
+
+            return response()->json(['message' => 'Cadastro concluido com suseso'], 200);
+        } catch (\Exception $e) {
+            return response()->json($e->getMessage(), 401);
+            // $message = new ApiMessages($e->getMessage());
+            // return response()->json($message->getMessage(), 401);
+        }
+    }
+
+    public function update($id, UserRequest $request) {
+        $data = $request->all();
+
+        if($request->has('password') && $request->get('password')) {
+            $data['password'] = bcrypt($data['password']);
+        } else {
+            unset($data['password']);
+        }
+
+        try {
+
+            $user = $this->user->findOrFail($id);
+            $user->update($data);
+
+            return response()->json([
+                'data' => [
+                    'msg' => 'Cadastro atualizado com suseso'
+                ]
+            ], 200);
+        } catch (\Exception $e) {
+            return response()->json($e->getMessage(), 401);
+            // $message = new ApiMessages($e->getMessage());
+            // return response()->json($message->getMessage(), 401);
+        }
+    }
+
+    public function destroy($id) {
+
+        try {
+
+            $user = $this->user->findOrFail($id);
+            $user->delete();
+
+            return response()->json(['message' => 'Cadastro removido com suseso'], 200);
+        } catch (\Exception $e) {
+            return response()->json($e->getMessage(), 401);
+            // $message = new ApiMessages($e->getMessage());
+            // return response()->json($message->getMessage(), 401);
+        }
+    }
+}
